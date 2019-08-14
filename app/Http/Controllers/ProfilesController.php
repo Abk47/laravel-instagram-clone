91546b7f7033c59fb3bf470ff07eb17462bb59a7
Abuka <maacsgaming@gmail.com>
2019-08-15 01:06:34 +0300

    add features and caching

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -4,14 +4,34 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 use Intervention\Image\Facades\Image;
 
 class ProfilesController extends Controller
 {
     public function index(User $user)
     {
+        // Learning how to use Cache in my application
+        $postCount = Cache::remember('counts.post.' . $user->id,
+         now()->addSeconds(30), 
+         function() use($user){
+            return  $user->posts->count();
+        }); 
+
+        $followersCount = Cache::remember('counts.followers.' . $user->id,
+         now()->addSeconds(30), 
+         function() use($user){
+            return   $user->profile->followers->count();
+        }); 
+
+        $followingCount = Cache::remember('counts.following.' . $user->id,
+         now()->addSeconds(30), 
+         function() use($user){
+            return  $user->following->count();
+        }); 
+
         $follows = (auth()->user()) ? auth()->user()->following->contains($user->id) : false; // Checks following count
-        return view('profiles.index', compact('user', 'follows'));
+        return view('profiles.index', compact('user', 'follows', 'postCount', 'followersCount', 'followingCount'));
     }
 
     public function edit(User $user)
